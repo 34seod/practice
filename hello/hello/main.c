8e1206b8ca1e351d5e17fd162bd908101f718a44
btpink-seo <btpink.seo@gmail.com>
2020-03-16 19:51:25 +0900

    hello

---
+++ hello/hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by btpink on 2020/03/16.
+//  Copyright © 2020 서상현. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
